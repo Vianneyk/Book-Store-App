8cabda39d28b58dc3dbfcaab19ad54cdf28b4f1a
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-10-19 02:41:33 +0100

    set up hooks and checked the response from api data

--- client/src/Books.js
+++ client/src/Books.js
@@ -2,7 +2,21 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Books = () => {
-	return <div></div>;
+	const [books, setBooks] = useState([]);
+
+	useEffect(() => {
+		const fetchBooks = async () => {
+			const res = await axios.get(
+				"https://www.googleapis.com/books/v1/volumes?q=HTML5"
+			);
+
+			console.log(res.data.items);
+		};
+
+		fetchBooks();
+	}, []);
+
+	return;
 };
 
 export default Books;
