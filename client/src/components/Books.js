b6c8ce91302c82599185deb55f9777c28edd1a38
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-10-21 12:51:21 +0100

    deleted comments

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -15,11 +15,10 @@ const Books = () => {
 
 		fetchBooks();
 	}, []);
-
 	return (
 		<>
-			<h1>The Book Store</h1>
-			<section>
+			<h1 className="font-bold text-center text-4xl py-5">The Book Store</h1>
+			<section className="grid grid-cols-2 gap-10 px-5">
 				{books.map(book => {
 					const {
 						id,
@@ -33,16 +32,23 @@ const Books = () => {
 					} = book;
 
 					return (
-						<article key={id}>
-							<div>
-								<img src={thumbnail} alt={title} />
+						<article
+							key={id}
+							className="md:flex bg-gray-200 rounded-xl p-8 md:p-0"
+						>
+							<div className="lg:flex lg:items-center">
+								<img
+									className="rounded-t-lg h-60 w-full object-cover"
+									src={thumbnail}
+									alt={title}
+								/>
 							</div>
 
-							<div>
-								<h3>{title}</h3>
-								<span>Author:</span> {authors}
-								<p>Pages: {pageCount}</p>
-								<p>{description.substring(0, 140)}</p>
+							<div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+								<h3 className="font-bold my-2 text-2xl">{title}</h3>
+								<span className="font-bold">Author:</span> {authors}
+								<p className="mb-4">Pages: {pageCount}</p>
+								<p className="mb-4">{description.substring(0, 140)}</p>
 							</div>
 						</article>
 					);
